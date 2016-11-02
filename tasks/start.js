550e0d8c2719e67e07ee49b623d3d183a4c0d3a2
Patrick King <patrick.king@vizworx.com>
2016-11-02 12:05:54 -0700

    Environment variables + windows == sadness.

--- tasks/start.js
+++ tasks/start.js
@@ -1,15 +1,23 @@
 var execSync = require('child_process').execSync;
+var exec = require('child_process').exec;
+var os = require('os');
 
-var node_env;
-var command = "node ./www";
+var node_env, process;
+var command = "node ./tasks/www";
 
-if (os.paltform() === 'win32') {
+if (os.platform() === 'win32') {
 // Node returns 'win32' for both 32 and 64 bit windows
-  execSync('set NODE_ENV=development');
-  execSync(command);
+  //process = execSync('cmd /V /C "set "NODE_ENV=development" && ' + command + '"');
+  process = exec(command);
 }
 else {
-  execSync('NODE_ENV=development ' + command);
+  process = exec('NODE_ENV=development ' + command);
 }
 
+process.stdout.on('data', function(data){
+  console.log(data.toString())
+})
 
+process.stderr.on('data', function(data){
+  console.log(data.toString())
+})
--- tasks/watch.js
+++ tasks/watch.js
@@ -1,1 +1,24 @@
-"NODE_ENV=development watchify JS/App.coffee -t coffeeify --extension='.coffee' -t [ stringify --extensions [.mustache .css .json] ] -t envify -o public/bundle.js --debug --verbose",
+var execSync = require('child_process').execSync;
+var os = require('os');
+
+var node_env;
+var command = "watchify JS/App.coffee -t coffeeify --extension='.coffee' -t [ stringify --extensions [.mustache .css .json] ] -t envify -o public/bundle.js --debug --verbose";
+
+if (os.platform() === 'win32') {
+// Node returns 'win32' for both 32 and 64 bit windows
+  execSync('cmd /V /C "set "NODE_ENV=development" && ' + command + '"');
+  // execSync(command);
+}
+else {
+  execSync('NODE_ENV=development ' + command);
+}
+
+
+
+
+
+
+
+
+
+
