9157ecf19997192e9d1f269f995626897a61e846
Steven Rothenburger <steven.rothenburger@vizworx.com>
2020-08-05 08:06:18 -0600

    refactor(Horizontal Control Bar):  update HCB to better reflect new design

--- src/components/HorizontalControlBar/index.jsx
+++ src/components/HorizontalControlBar/index.jsx
@@ -1,8 +1,8 @@
 // #region imports
-import React, { useContext, useMemo, useEffect } from 'react';
+import React, { useContext, useMemo, useEffect, useState } from 'react';
 import {
   makeStyles, createStyles,
-  Grid, Typography, Fab, IconButton, Button,
+  Grid, Typography,
 } from '@material-ui/core';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
@@ -14,61 +14,51 @@ import { CONFIG_LAYOUT } from '../../constants';
 
 const useStyles = makeStyles(theme => createStyles({
   root: props => ({
-    width: props.width || '100%',
+    width: props.width || '90%',
     '& > div': { textAlign: 'center' },
-    marginLeft: '20px',
+    paddingLeft: '20px',
+    marginLeft: '0px',
     paddingTop: '12px',
+    paddingBottom: '10px',
+    backgroundColor: '#F3EFEF',
+    height: '40px',
   }),
-  productionButton: {
-    height: 10,
-    width: 10,
-  },
-  subtitle: {
-    marginTop: theme.spacing(0.75),
-  },
-  totalDemandButton: {
-    borderRadius: 0,
-    backgroundColor: '#e0e0e0',
-    border: 'none',
-    minWidth: '70px',
-    width: '70px',
-    color: '#696985',
-    fontWeight: '600',
-  },
-  chooseProdLabel: {
-    minWidth: '60px',
-    width: '60px',
-    alignItems: 'center',
-    fontWeight: '500',
-    marginRight: '30px',
-  },
   selectUnitLabel: {
     minWidth: '60px',
-    width: '60px',
+    width: '125px',
     textAlign: 'center',
+    fontWeight: '900',
+    fontSize: '14pt',
+    color: '#4A93C7',
+    fontFamily: 'Roboto Condensed, Helvetica, Arial, sans-serif',
   },
-  chooseDemandLabel: {
+  viewByLabel: {
     marginRight: 8,
-    minWidth: 70,
+    minWidth: 80,
     width: 70,
     border: 'none',
-    fontWeight: '500',
-  },
-  unitButtonSelected: {
-    borderRadius: 0,
-    height: '30px',
-    color: 'white',
-    backgroundColor: '#898989',
     fontWeight: 'bold',
-    border: '0px',
+    fontSize: '14pt',
+    color: '#4A93C7',
+    fontFamily: 'Roboto Condensed, Helvetica, Arial, sans-serif',
   },
-  unitButtonUnselected: {
+  buttonSelected: {
+    borderRadius: '0 !important',
+    height: '28px !important',
+    color: 'white !important',
+    backgroundColor: '#4A93C7 !important',
+    fontWeight: '900 !important',
+    border: '0px !important',
+    fontFamily: 'Roboto Condensed, Helvetica, Arial, sans-serif',
+  },
+  buttonUnselected: {
     borderRadius: 0,
-    height: '30px',
+    height: '28px',
     color: 'black',
     backgroundColor: '#ffffff',
-    fontWeight: 'bold',
+    fontWeight: '900',
     border: '1px solid #898989',
+    fontFamily: 'Roboto Condensed, Helvetica, Arial, sans-serif',
   },
 }));
 
@@ -77,6 +67,9 @@ const HorizontalControlBar = () => {
   const { config, setConfig } = useContext(ConfigContext);
   const layout = useMemo(() => CONFIG_LAYOUT[config.mainSelection], [config.mainSelection]);
   const handleConfigUpdate = (field, value) => () => setConfig({ ...config, [field]: value });
+
+  // TODO: Make this more meaningful than just a state object.
+  const [selectedView, setSelectedView] = useState('region');
 
   /**
    * If the current selected unit is no longer available under the new source,
@@ -90,56 +83,59 @@ const HorizontalControlBar = () => {
   if (!layout) {
     return null;
   }
-  // #region Buttons
-  const demandButtons = Object.keys(CONFIG_LAYOUT).map((source) => {
-    const Icon = CONFIG_REPRESENTATION[source].icon;
-    return (
-      config.mainSelection === source ? (
-        <Fab color="inherit" onClick={handleConfigUpdate('mainSelection', source)}><Icon fontSize="large" /></Fab>
-      ) : (
-        <IconButton color="inherit" onClick={handleConfigUpdate('mainSelection', source)} className={classes.productionButton}>
-          <Icon fontSize="large" />
-        </IconButton>
-      )
-    );
-  });
 
   const handleUnitUpdate = (event, newUnit) => {
     handleConfigUpdate('unit', newUnit);
   };
+  const handleViewUpdate = (event, newView) => {
+    setSelectedView(newView);
+  };
 
   const unitButtons = layout.unit.map((unit) => {
     const styles = (unit === config.unit)
-      ? classes.unitButtonSelected
-      : classes.unitButtonUnselected;
+      ? classes.buttonSelected
+      : classes.buttonUnselected;
     return (
       <ToggleButton
         value={unit}
         className={styles}
         key={`${unit}-button`}
       >
-        {CONFIG_REPRESENTATION[unit]}
+        <Typography>
+          {CONFIG_REPRESENTATION[unit]}
+        </Typography>
       </ToggleButton>
     );
   });
-  // #endregion
+
+  const viewButtons = ['region', 'source'].map((view) => {
+    const styles = (view === selectedView)
+      ? { classes: { selected: classes.buttonSelected } }
+      : { className: classes.buttonUnselected };
+    return (
+      <ToggleButton
+        value={view}
+        {...styles}
+        key={`${view}-button`}
+      >
+        <Typography>{view}</Typography>
+      </ToggleButton>
+    );
+  });
 
   return (
     <Grid container alignItems="center" wrap='nowrap' spacing={5} className={classes.root}>
-      <Typography className={classes.chooseDemandLabel}>CHOOSE DEMAND</Typography>
-      <Button
-        className={classes.totalDemandButton}
-        onClick={handleConfigUpdate('mainSelection', 'energyDemand')}
-      >TOTAL DEMAND
-      </Button>
-      <Grid item><Typography variant="body1" style={{ fontSize: 25 }}>OR</Typography></Grid>
-      <Grid item><Typography variant="body1" className={classes.chooseProdLabel}>CHOOSE PRODUCTION</Typography></Grid>
-
-      <Grid item>{demandButtons[1]}</Grid>
-      <Grid item>{demandButtons[2]}</Grid>
-      <Grid item>{demandButtons[3]}</Grid>
+      <Typography className={classes.viewByLabel} variant='body1'>VIEW BY</Typography>
+      <ToggleButtonGroup
+        value={selectedView}
+        onChange={handleViewUpdate}
+        exclusive
+        style={{ paddingRight: '90px' }}
+      >
+        {viewButtons}
+      </ToggleButtonGroup>
       <Grid item>
-        <Typography variant="body1" className={classes.selectUnitLabel}>SELECT UNIT</Typography>
+        <Typography className={classes.selectUnitLabel}>SELECT UNIT</Typography>
       </Grid>
       <ToggleButtonGroup
         value={config.unit}
