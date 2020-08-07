6ba652946903097d18c6f0a38e30c631787f918a
Steven Rothenburger <steven.rothenburger@vizworx.com>
2020-08-07 14:07:57 -0600

    feat(useEnergyFutureData): added-hook-to-retrieve-data

--- src/pages/ByRegion/index.jsx
+++ src/pages/ByRegion/index.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useMemo, useCallback } from 'react';
 import { ResponsiveBar } from '@nivo/bar';
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+// import useEnergyFutureData from '../../useEnergyFutureData';
 import { data as dataEnergyDemand } from './dataEnergyDemand';
 import { data as dataElectricityGeneration } from './dataElectricityGeneration';
 import { data as dataOilProduction } from './dataOilProduction';
@@ -10,22 +9,11 @@ import { data as dataGasProduction } from './dataGasProduction';
 import { ConfigContext } from '../../containers/App/lazy';
 import { CONFIG_REPRESENTATION } from '../../types';
 
-
-const RESOURCES = gql`
-  query {
-    energyDemands {
-      region
-      value: quantity
-      year
-    }
-  }
-`;
-
 const ByRegion = () => {
   const { config } = useContext(ConfigContext);
 
-  //const { loading, error, data: response } = useQuery(RESOURCES);
-  //console.log('GraphQL example:', loading, error, response);
+  // const gqlConfig = { page: 'energyDemand', scenario: 'reference', iteration: 1, sector: 'total end-use', source: 'ALL' };
+  // console.log(useEnergyFutureData(gqlConfig));
 
   const data = useMemo(() => {
     switch (config.mainSelection) {
@@ -37,9 +25,9 @@ const ByRegion = () => {
   }, [config.mainSelection]);
 
   const configFilter = useCallback(
-    row => config.provinces.indexOf(row.province) > -1 &&
-      (!row.unit || row.unit.toLowerCase() === CONFIG_REPRESENTATION[config.unit].toLowerCase()) &&
-      (!row.scenario || row.scenario === config.scenario),
+    row => config.provinces.indexOf(row.province) > -1
+      && (!row.unit || row.unit.toLowerCase() === CONFIG_REPRESENTATION[config.unit].toLowerCase())
+      && (!row.scenario || row.scenario === config.scenario),
     [config],
   );
 
@@ -63,7 +51,7 @@ const ByRegion = () => {
       margin={{ top: 50, right: 0, bottom: 50, left: 80 }}
       padding={0.1}
       colors={{ scheme: 'nivo' }}
-      borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+      borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
       axisTop={null}
       axisRight={null}
       axisBottom={{
@@ -72,7 +60,7 @@ const ByRegion = () => {
         tickRotation: 0,
         legendPosition: 'middle',
         legendOffset: 32,
-        format: year => (year % 5) ? '' : year,
+        format: year => ((year % 5) ? '' : year),
       }}
       axisLeft={{
         tickSize: 5,
@@ -85,7 +73,7 @@ const ByRegion = () => {
       // labelSkipWidth={12}
       // labelSkipHeight={12}
       // labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-      animate={true}
+      animate
       motionStiffness={90}
       motionDamping={15}
     />
