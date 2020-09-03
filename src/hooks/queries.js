66a11bc9cd0f625153103fa784025e628375f843
Steven Rothenburger <steven.rothenburger@vizworx.com>
2020-09-03 11:09:29 -0600

    fix(queries): fixed formatting.

--- src/hooks/queries.js
+++ src/hooks/queries.js
@@ -10,55 +10,57 @@ export const ENERGY_DEMAND = gql`
     }
   }
 `;
+
 export const GAS_PRODUCTIONS = gql`
-query ($iteration: ID!, $regions: [Region!], $scenarios: [String!]) {
-  resources:gasProductions(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios, sources: [ALL] ){
+  query ($iteration: ID!, $regions: [Region!], $scenarios: [String!]) {
+    resources:gasProductions(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios, sources: [ALL] ){
+        province: region
+        year
+        scenario
+        value: quantity
+      }
+    }
+  `;
+
+export const ELECTRICITY_GENERATIONS = gql`
+  query ($iteration: ID!, $regions: [Region!], $scenarios: [String!]) {
+    resources:electricityGenerations(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios, sources: [ALL]) {
       province: region
       year
       scenario
       value: quantity
     }
   }
-`;
+  `;
 
-export const ELECTRICITY_GENERATIONS = gql`
-query ($iteration: ID!, $regions: [Region!], $scenarios: [String!]) {
-  resources:electricityGenerations(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios, sources: [ALL]) {
-    province: region
-    year
-    scenario
-    value: quantity
-  }
-}
-`;
 export const OIL_PRODUCTIONS = gql`
-query ($iteration: ID!, $regions: [Region!], $scenarios: [String!]) {
-  resources:oilProductions(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios) {
-    province: region
-    year
-    value: quantity
+  query ($iteration: ID!, $regions: [Region!], $scenarios: [String!]) {
+    resources:oilProductions(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios) {
+      province: region
+      year
+      value: quantity
+    }
   }
-}
-`;
+  `;
 
 export const BY_SECTOR = gql`
-query ($iteration: ID!, $regions: [Region!], $scenarios: [String!], $sectors:[String!], $sources: [EnergySource!]) {
-  resources:energyDemands(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios, sectors: $sectors, sources: $sources) {
-    year
-    value: quantity
-    source
+  query ($iteration: ID!, $regions: [Region!], $scenarios: [String!], $sectors:[String!], $sources: [EnergySource!]) {
+    resources:energyDemands(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios, sectors: $sectors, sources: $sources) {
+      year
+      value: quantity
+      source
+    }
   }
-}
-`;
+  `;
 
 export const ELECTRICITY_GENERATIONS_SOURCE = gql`
-query ($iteration: ID!, $regions: [Region!], $scenarios: [String!]) {
-  resources:electricityGenerations(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios, sources: []) {
-    province: region
-    year
-    source
-    value: quantity
+  query ($iteration: ID!, $regions: [Region!], $scenarios: [String!]) {
+    resources:electricityGenerations(iterationIds: [$iteration], regions: $regions, scenarios: $scenarios, sources: []) {
+      province: region
+      year
+      source
+      value: quantity
+    }
   }
-}
-`;
+  `;
 export const NULL_QUERY = gql`{ _ }`;
