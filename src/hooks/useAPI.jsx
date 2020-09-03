db042c9fa59341818b1661e866a26a7239cd3a84
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2020-09-03 14:49:58 -0600

    feat: Added translations from the API to the i18n messages
    B-07551

--- src/hooks/useAPI.jsx
+++ src/hooks/useAPI.jsx
@@ -9,6 +9,12 @@ const CONFIG = gql`
       year
       scenarios
     }
+    translations {
+      group
+      key
+      english
+      french
+    }
   }
 `;
 
@@ -28,12 +34,70 @@ const getYearIdIterations = iterations => (
   }, {})
 );
 
+const getI18NMessages = translations => (
+  translations.reduce((i18nMessages, translation) => {
+    let key;
+
+    switch (translation.group) {
+      case 'REGION':
+        key = `regions.${translation.key}`;
+        break;
+      case 'SCENARIO':
+        key = `components.scenarioSelect.${translation.key}.title`;
+        break;
+      case 'ELECTRICITY_SOURCE':
+        key = `common.sources.electricity.${translation.key}`;
+        break;
+      case 'ENERGY_SOURCE':
+        key = `common.sources.energy.${translation.key}`;
+        break;
+      case 'GAS_SOURCE':
+        key = `common.sources.gas.${translation.key}`;
+        break;
+      case 'OIL_SOURCE':
+        key = `common.sources.oil.${translation.key}`;
+        break;
+      case 'SECTOR':
+        key = `components.horizontalControlBar.${translation.key}`;
+        break;
+      default:
+        return i18nMessages;
+    }
+
+    // eslint-disable-next-line no-param-reassign
+    i18nMessages.en[key] = translation.english;
+    // eslint-disable-next-line no-param-reassign
+    i18nMessages.fr[key] = translation.french;
+
+    return i18nMessages;
+  }, { en: {}, fr: {} })
+);
+
 export default () => {
   const { loading, error, data } = useQuery(CONFIG);
   const yearIdIterations = useMemo(
     () => (data ? getYearIdIterations(data.iterations) : {}),
     [data],
   );
+  // TODO
+  const regions = useMemo(
+    () => (data ? {} : {}),
+    [data],
+  );
+  // TODO
+  const sources = useMemo(
+    () => (data ? {} : {}),
+    [data],
+  );
+  // TODO
+  const sectors = useMemo(
+    () => (data ? {} : {}),
+    [data],
+  );
+  const translations = useMemo(
+    () => (data ? getI18NMessages(data.translations) : {}),
+    [data],
+  );
 
-  return { loading, error, yearIdIterations };
+  return { loading, error, yearIdIterations, regions, sources, sectors, translations };
 };
