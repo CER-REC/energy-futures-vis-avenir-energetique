de03cb1299960a400d4563cc527f608631dc08f0
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2020-09-14 16:17:26 -0600

    feat: Added loading of API translations for Storybook
    B-07551

--- .storybook/config.jsx
+++ .storybook/config.jsx
@@ -1,9 +1,13 @@
-import requireContext from 'require-context.macro';
 import React from 'react';
-import { setIntlConfig, withIntl } from 'storybook-addon-intl';
 import { addDecorator, configure, addParameters } from '@storybook/react';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure as enzyme } from 'enzyme';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+import requireContext from 'require-context.macro';
+import { setIntlConfig, withIntl } from 'storybook-addon-intl';
 import { addReadme, configureReadme } from 'storybook-readme';
 
 // Load Locale Data
@@ -16,23 +20,12 @@ import '@formatjs/intl-relativetimeformat/locale-data/fr';
 
 import { lang } from '../src/constants';
 import i18nMessages from '../src/i18n';
+import getI18NMessages from '../src/utilities/getI18NMessages';
 
-setIntlConfig({
-  locales: ['en', 'fr'],
-  defaultLocale: lang,
-  getMessages: locale => i18nMessages[locale],
-});
-
-addDecorator(withIntl);
-
-addDecorator(addReadme);
-configureReadme({
-  // eslint-disable-next-line react/prop-types
-  DocPreview: ({ children }) => (
-    <div style={{ padding: '40px 40px 0' }}> {children}</div>
-  ),
-});
-
+const cache = new InMemoryCache();
+const link = new HttpLink({ uri: '/energy-future/graphql' });
+const client = new ApolloClient({ cache, link, fetch });
+const locales = Object.keys(i18nMessages);
 const viewports = {
   fullscreen: {
     name: 'Fullscreen',
@@ -51,42 +44,75 @@ const viewports = {
     styles: { width: '768px', height: '100%' },
   },
 };
-addParameters({ viewport: { viewports, defaultViewport: 'fullscreen' } });
 
-addParameters({
-  options: {
-    brandTitle: 'Energy Future 2.0 DevDoc',
-    panelPosition: 'bottom',
-  },
+client.query({
+  query: gql`
+    query {
+      translations {
+        group
+        key
+        english
+        french
+      }
+    }
+  `,
+}).then((result) => {
+  const apiI18NMessages = getI18NMessages(result.data.translations);
+  const messages = {};
+
+  locales.forEach((locale) => {
+    messages[locale] = { ...apiI18NMessages[locale], ...i18nMessages[locale] };
+  });
+
+  setIntlConfig({
+    locales,
+    defaultLocale: lang,
+    getMessages: locale => messages[locale],
+  });
+}).catch(() => {
+  setIntlConfig({
+    locales,
+    defaultLocale: lang,
+    getMessages: locale => i18nMessages[locale],
+  });
+
+  // eslint-disable-next-line no-alert
+  alert('Unable to load API translations.');
+}).finally(() => {
+  // Automatically import all files named stories.jsx
+  const documentationStories = requireContext('../documentation/', true, /stories.jsx$/);
+  const componentStories = requireContext('../src/', true, /stories.jsx$/);
+
+  addDecorator(withIntl);
+  addDecorator(addReadme);
+
+  configureReadme({
+    // eslint-disable-next-line react/prop-types
+    DocPreview: ({ children }) => (
+      <div style={{ padding: '40px 40px 0' }}> {children}</div>
+    ),
+  });
+
+  addParameters({ viewport: { viewports, defaultViewport: 'fullscreen' } });
+  addParameters({
+    options: {
+      brandTitle: 'Energy Future 2.0 DevDoc',
+      panelPosition: 'bottom',
+    },
+  });
+
+  addDecorator((storyFn, context) => {
+    if (context.id === 'containers-app--within-wet') { return storyFn(); }
+    return <div className="visualization">{storyFn()}</div>;
+  });
+
+  enzyme({ adapter: new Adapter() });
+
+  configure(() => {
+    documentationStories.keys()
+      // Sorting Documentation|Introduction to the top
+      .sort((a, b) => (a.startsWith('./Introduction/') ? -1 : a.localeCompare(b)))
+      .forEach(filename => documentationStories(filename));
+    componentStories.keys().forEach(filename => componentStories(filename));
+  }, module);
 });
-
-addDecorator((storyFn, context) => {
-  if (context.id === 'containers-app--within-wet') { return storyFn(); }
-  return <div className="visualization">{storyFn()}</div>;
-});
-
-// automatically import all files named stories.jsx
-const documentationStories = requireContext('../documentation/', true, /stories.jsx$/);
-const componentStories = requireContext('../src/', true, /stories.jsx$/);
-const containerOrder = [
-  './containers/ViewOne/stories.jsx',
-  './containers/ViewTwo/stories.jsx',
-  './containers/ViewThree/stories.jsx',
-  './containers/Footer/stories.jsx',
-];
-function loadStories() {
-  documentationStories.keys()
-    // Sorting Documentation|Introduction to the top
-    .sort((a, b) => (a.startsWith('./Introduction/') ? -1 : a.localeCompare(b)))
-    .forEach(filename => documentationStories(filename));
-  componentStories.keys()
-    .sort((a, b) => {
-      if (!a.startsWith('./containers/') || !b.startsWith('./containers/')) { return 0; }
-      // This is a container, so sort it by usage
-      return containerOrder.indexOf(a) - containerOrder.indexOf(b);
-    })
-    .forEach(filename => componentStories(filename));
-}
-
-enzyme({ adapter: new Adapter() });
-configure(loadStories, module);
