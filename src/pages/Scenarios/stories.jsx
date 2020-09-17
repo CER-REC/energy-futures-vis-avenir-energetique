9d88327cc4d17a8b88081586ae3a4e900db639e7
Steven Rothenburger <steven.rothenburger@vizworx.com>
2020-09-17 12:25:58 -0600

    fix(scenarrios):added config hook to story

--- src/pages/Scenarios/stories.jsx
+++ src/pages/Scenarios/stories.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesForComponent } from '../../../.storybook/utils';
 import Scenarios from './index';
 import ReadMe from './README.md';
+import withConfigAndGQL from '../../../.storybook/addon-config-and-gql';
 
 storiesForComponent('Pages|Scenarios', module, ReadMe)
+  .addDecorator(withConfigAndGQL)
   .add('default', () => <Scenarios />);
