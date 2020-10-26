4bc3969bded86b25093762ab8a40a6c3b273ded5
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2020-10-26 11:05:40 -0600

    feat: Added email share contents
    B-07704

--- src/components/Share/index.jsx
+++ src/components/Share/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import LinkIcon from '@material-ui/icons/Link';
 import EmailIcon from '@material-ui/icons/Email';
+import { useIntl } from 'react-intl';
 
 import { IconTwitter, IconFacebook, IconLinkedIn } from '../../icons';
 import LinkButtonGroup from '../LinkButtonGroup';
@@ -19,7 +20,11 @@ const getBitlyURL = () => {
     }).catch(() => document.location.href);
 };
 
-const openShareWindow = baseUrl => getBitlyURL().then(bitlyUrl => window.open(`${baseUrl}${bitlyUrl}`, 'targetWindow', 'width=650,height=650'));
+const openShareWindow = baseUrl => getBitlyURL().then(bitlyUrl => window.open(
+  `${baseUrl}${bitlyUrl}`,
+  'targetWindow',
+  'width=650,height=650',
+));
 
 const linkedin = {
   name: 'linkedin',
@@ -39,17 +44,35 @@ const twitter = {
   content: () => openShareWindow('https://twitter.com/intent/tweet?url='),
 };
 
-const Share = () => (
-  <LinkButtonGroup
-    labels={[[{ name: 'download data' }], [
-      { icon: <LinkIcon />, name: 'Copy Link', content: () => {} },
-      linkedin,
-      facebook,
-      twitter,
-      { icon: <EmailIcon />, name: 'Email', content: () => {} },
-    ]]}
-    accent="right"
-  />
-);
+const Share = () => {
+  const intl = useIntl();
+  const email = useMemo(() => ({
+    name: 'email',
+    icon: <EmailIcon />,
+    content: () => {
+      getBitlyURL().then((bitlyUrl) => {
+        const subject = intl.formatMessage({ id: 'components.share.emailSubject' });
+        const message = intl.formatMessage({ id: 'components.share.emailMessage' });
+        const body = `${encodeURIComponent(bitlyUrl)}%0A%0A${encodeURIComponent(message)}`;
+        const emailUrl = `mailto:?subject=${encodeURIComponent(subject)}&body=${body}`;
+
+        window.location.href = emailUrl;
+      });
+    },
+  }), [intl]);
+
+  return (
+    <LinkButtonGroup
+      labels={[[{ name: 'download data' }], [
+        { icon: <LinkIcon />, name: 'Copy Link', content: () => {} },
+        linkedin,
+        facebook,
+        twitter,
+        email,
+      ]]}
+      accent="right"
+    />
+  );
+};
 
 export default Share;
