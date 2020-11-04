e73998fd560e3a20f18ea579522380021e3548f5
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2020-11-03 17:35:00 -0700

    feat: Added source and source order actions to the config reducer
    B-07744

--- src/hooks/useConfig.jsx
+++ src/hooks/useConfig.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { createBrowserHistory } from 'history';
 import queryString from 'query-string';
 
-import { CONFIG_LAYOUT } from '../constants';
+import { CONFIG_LAYOUT, PAGES } from '../constants';
 import useAPI from './useAPI';
 
 const parameters = ['page', 'mainSelection', 'yearId', 'sector', 'unit', 'view'];
@@ -30,7 +30,7 @@ const initialState = {
   sourceOrder: null,
 };
 
-const getReducer = (regions, yearIdIterations) => {
+const getReducer = (regions, sources, yearIdIterations) => {
   const getSelection = (page, selection) => {
     const validSelections = Object.keys(CONFIG_LAYOUT).filter(
       selectionKey => CONFIG_LAYOUT[selectionKey].pages.includes(page),
@@ -61,9 +61,7 @@ const getReducer = (regions, yearIdIterations) => {
     }
 
     const validScenarios = yearIdIterations[yearId].scenarios;
-    const validatedScenarios = validScenarios.filter(
-      scenario => scenarios && (scenarios.indexOf(scenario) !== -1),
-    );
+    const validatedScenarios = validScenarios.filter(scenario => scenarios?.includes(scenario));
 
     if (validatedScenarios.length) {
       return (page === 'scenarios') ? validatedScenarios : [validatedScenarios[0]];
@@ -77,26 +75,69 @@ const getReducer = (regions, yearIdIterations) => {
       return null;
     }
 
-    const validatedProvinces = regions.filter(region => provinces?.includes(region));
+    const validatedProvinces = regions.order.filter(region => provinces?.includes(region));
 
     if ((page === 'by-region') || ((page === 'electricity') && (view === 'source'))) {
-      return provinces?.includes('ALL') ? regions : validatedProvinces;
+      return (provinces?.includes('ALL') || !provinces) ? regions.order : validatedProvinces;
     }
 
-    if (provinces?.includes('ALL') || (validatedProvinces.length === regions.length)) {
+    if (
+      provinces?.includes('ALL')
+      || !validatedProvinces.length
+      || (validatedProvinces.length === regions.order.length)
+    ) {
       return ['ALL'];
     }
 
-    return validatedProvinces.length ? [validatedProvinces[0]] : regions;
+    return [validatedProvinces[0]];
   };
   const getProvinceOrder = (page, order) => {
     if (page === initialState.page) {
       return null;
     }
 
-    const validatedProvinces = regions.filter(region => order?.includes(region));
+    const validatedProvinces = regions.order.filter(region => order?.includes(region));
 
-    return validatedProvinces.length === regions.length ? order : regions;
+    return (validatedProvinces.length === regions.order.length) ? order : regions.order;
+  };
+  const getValidSources = (page, selection) => {
+    const type = PAGES.find(pageItem => pageItem.id === page).sourceTypes?.[selection];
+
+    return sources[type]?.order;
+  };
+  const getSources = (page, selection, view, selectedSources) => {
+    const validSources = getValidSources(page, selection);
+
+    if (!validSources) {
+      return null;
+    }
+
+    const validatedSources = validSources.filter(source => selectedSources?.includes(source));
+
+    if ((page === 'by-sector') || ((page === 'electricity') && (view === 'region'))) {
+      return (selectedSources?.includes('ALL') || !selectedSources) ? validSources : validatedSources;
+    }
+
+    if (
+      selectedSources?.includes('ALL')
+      || !validatedSources.length
+      || (validatedSources.length === validSources.length)
+    ) {
+      return ['ALL'];
+    }
+
+    return [validatedSources[0]];
+  };
+  const getSourceOrder = (page, selection, order) => {
+    const validSources = getValidSources(page, selection);
+
+    if (!validSources) {
+      return null;
+    }
+
+    const validatedSources = validSources.filter(source => order?.includes(source));
+
+    return (validatedSources.length === validSources.length) ? order : validSources;
   };
 
   return (state, action) => {
@@ -123,11 +164,11 @@ const getReducer = (regions, yearIdIterations) => {
           view,
           sector: getSector(action.payload, state.sector),
           scenarios: getScenarios(action.payload, yearId, state.scenarios),
-          // Default to all the regions
-          provinces: getProvinces(action.payload, view, state.provinces || regions),
+          provinces: getProvinces(action.payload, view, state.provinces),
           provinceOrder: getProvinceOrder(action.payload, state.provinceOrder),
-          sources: [],
-          sourceOrder: [],
+          // Always reset the sources on page change
+          sources: getSources(action.payload, mainSelection, view, null),
+          sourceOrder: getSourceOrder(action.payload, mainSelection, state.sourceOrder),
         };
       case 'mainSelection/changed':
         mainSelection = getSelection(state.page, action.payload);
@@ -136,8 +177,8 @@ const getReducer = (regions, yearIdIterations) => {
           ...state,
           mainSelection,
           unit: getUnit(mainSelection, state.unit),
-          sources: [],
-          sourceOrder: [],
+          sources: getSources(state.page, mainSelection, state.view, state.sources),
+          sourceOrder: getSourceOrder(state.page, mainSelection, state.sourceOrder),
         };
       case 'yearId/changed':
         yearId = getYearId(state.page, action.payload);
@@ -154,21 +195,19 @@ const getReducer = (regions, yearIdIterations) => {
           unit: getUnit(state.mainSelection, action.payload),
         };
       case 'view/changed':
-        view = getView(state.page, action.view);
+        view = getView(state.page, action.payload);
 
         return {
           ...state,
           view,
           provinces: getProvinces(state.page, view, state.provinces),
-          sources: [],
-          sourceOrder: [],
+          sources: getSources(state.page, state.mainSelection, view, state.sources),
+          sourceOrder: getSourceOrder(state.page, state.mainSelection, state.sourceOrder),
         };
       case 'sector/changed':
         return {
           ...state,
-          sector: getSector(state.sector, action.payload),
-          sources: [],
-          sourceOrder: [],
+          sector: getSector(state.page, action.payload),
         };
       case 'scenarios/changed':
         return {
@@ -188,12 +227,12 @@ const getReducer = (regions, yearIdIterations) => {
       case 'sources/changed':
         return {
           ...state,
-          sources: action.payload || [],
+          sources: getSources(state.page, state.mainSelection, state.view, action.payload),
         };
       case 'sourceOrder/changed':
         return {
           ...state,
-          sourceOrder: action.payload || [],
+          sourceOrder: getSourceOrder(state.page, state.mainSelection, action.payload),
         };
       default:
         return state;
@@ -202,8 +241,11 @@ const getReducer = (regions, yearIdIterations) => {
 };
 
 export const ConfigProvider = ({ children }) => {
-  const { regions: { order: regions }, yearIdIterations } = useAPI();
-  const reducer = useMemo(() => getReducer(regions, yearIdIterations), [regions, yearIdIterations]);
+  const { regions, sources, yearIdIterations } = useAPI();
+  const reducer = useMemo(
+    () => getReducer(regions, sources, yearIdIterations),
+    [regions, sources, yearIdIterations],
+  );
   const [config, configDispatch] = useReducer(reducer, initialState);
 
   /**
