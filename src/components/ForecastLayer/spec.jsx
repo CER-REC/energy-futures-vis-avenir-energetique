03d2d16cd75cc32a6a10ed831494a67703f80511
Nico Li <nico.li@vizworx.com>
2020-12-05 16:51:07 -0700

    feat(Testing): adds testing scripts for FadeLayer and ForecastLayer
    Issues Affected: B-07846

---
+++ src/components/ForecastLayer/spec.jsx
@@ -0,0 +1,38 @@
+import { mount } from 'enzyme';
+import forecastLayer from '.';
+
+const MOCK_YEAR = { year: { min: 2005, max: 2050, forecastStart: 2018 } };
+
+describe('Component|ForecastLayer', () => {
+  /**
+   * Chart with props 'innerHeight' and 'innerWidth'
+   */
+  describe('Test basic component', () => {
+    const wrapper = mount(forecastLayer(MOCK_YEAR)({ innerHeight: 600, innerWidth: 800 }));
+
+    test('should render component', () => {
+      expect(wrapper.type()).not.toBeNull();
+      expect(wrapper.findWhere(node => node.type() === 'text' && node.text() === 'forecast')).not.toBeNull();
+    });
+
+    test('should NOT render component without valid years', () => {
+      expect(forecastLayer({ year: undefined })()).toBeNull();
+    });
+  });
+
+  /**
+   * Chart with props 'height' and 'width'
+   */
+  describe('Test basic component', () => {
+    const wrapper = mount(forecastLayer(MOCK_YEAR)({ height: 600, width: 800 }));
+
+    test('should render component', () => {
+      expect(wrapper.type()).not.toBeNull();
+      expect(wrapper.findWhere(node => node.type() === 'text' && node.text() === 'forecast')).not.toBeNull();
+    });
+
+    test('should NOT render component without valid years', () => {
+      expect(forecastLayer({ year: undefined })()).toBeNull();
+    });
+  });
+});
