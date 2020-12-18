43f1a832c3eb9d4e0411ac6841fe24768ff50a04
Steven Rothenburger <steven.rothenburger@vizworx.com>
2020-12-18 13:13:50 -0700

    fix(Share): remove global nock

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -2,7 +2,6 @@ import registerRequireContextHook from 'babel-plugin-require-context-hook/regist
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import fetch from 'node-fetch';
-import nock from 'nock';
 import { monkeyPatchShallowWithIntl } from './utilities';
 
 jest.doMock('file-saver', () => ({ saveAs: jest.fn() }));
@@ -24,14 +23,3 @@ Object.defineProperty(global.window, 'location', {
     assign: global.location.assign,
   },
 });
-
-// Nock
-const bitlyDomain = 'http://localhost/bitlyService';
-const testUri = '/api/bitlyShortlink?shortenUrl=http%3A%2F%2Flocalhost%2F%3Fpage%3Dlanding%26mainSelection%3D%26yearId%3D%26sector%3D%26unit%3D%26view%3D%26baseYear%3D%26compareYear%3D%26noCompare%3D%26scenarios%3D%26provinces%3D%26provinceOrder%3D%26sources%3D%26sourceOrder%3D';
-
-nock(bitlyDomain, { allowUnmocked: true })
-  .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' })
-  .get(testUri)
-  .reply(200, {
-    data: { url: 'https://bit.ly/2W6hHBG' },
-  });
