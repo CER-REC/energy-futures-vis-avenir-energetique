955df58354872b7211c51edeb275fc75434ff190
Nico Li <nico.li@vizworx.com>
2021-01-07 11:49:21 -0700

    fix(Hooks): adds the updated mockApolloClient which is missing from the previous commit
    Issues Affected: B-07918

--- .storybook/mockApolloClient.js
+++ .storybook/mockApolloClient.js
@@ -1,11 +1,26 @@
-import { MockLink } from '@apollo/react-testing';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { MATCH_ANY_PARAMETERS, WildcardMockLink } from 'wildcard-mock-link';
 
-import mocks from '../src/tests/mocks';
+import * as queries from '../src/hooks/queries';
+import iterationsTranslations from '../src/tests/mocks/iterationsTranslations.json';
+import mockData from '../src/tests/mocks/data.json';
 
+/**
+ * mock the query that takes wild cards for testing the useEnergyFutureData hook
+ */
 const cache = new InMemoryCache({ addTypename: false });
-const link = new MockLink(mocks, false);
+const mocks = [
+  {
+    request: { query: queries.ITERATIONS_TRANSLATIONS },
+    result: iterationsTranslations,
+  },
+  ...Object.keys(queries).map(query => query !== 'ITERATIONS_TRANSLATIONS' && ({
+    request: { query: queries[query], variables: MATCH_ANY_PARAMETERS },
+    result: mockData,
+  })).filter(Boolean),
+];
+const link = new WildcardMockLink(mocks, false);
 const client = new ApolloClient({ cache, link });
 
 // Need to write data to cache for Storybook snapshots
