6db0dfa34d9c82df9112cd4bfde539f01e0303a7
diannayim <dianna.yim@gmail.com>
2023-06-05 10:08:39 -0600

    feat(YearSliceTooltip/NodeSection): updated props
    Issues Affected: 14179

--- src/components/YearSliceTooltip/NodeSection/index.jsx
+++ src/components/YearSliceTooltip/NodeSection/index.jsx
@@ -114,7 +114,7 @@ NodeSection.propTypes = {
       name: PropTypes.string,
       value: PropTypes.number,
       color: PropTypes.string,
-      mask: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+      mask: PropTypes.string,
     })),
     unit: PropTypes.string.isRequired,
     totalLabel: PropTypes.string,
