020cd378accdac40713e74128f7f157b6be5522e
diannayim <dianna.yim@gmail.com>
2023-06-13 14:57:01 -0600

    feat(PageSelect): fixed justify style for desktop
    Issues Affected: 14295

--- src/components/PageSelect/index.jsx
+++ src/components/PageSelect/index.jsx
@@ -127,7 +127,7 @@ const PageSelect = () => {
       container
       wrap="nowrap"
       spacing={1}
-      justify={!isDesktop ? 'space-around' : ''}
+      justify={!isDesktop ? 'space-around' : 'flex-start'}
       style={{ paddingTop: 10 }}
     >
       {pageButtons}
